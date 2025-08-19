4cb7ce71912a507297b06649e10467183ad16c66
Alex Woodman <alexthegoodman@gmail.com>
2025-08-19 19:03:52 -0400

    tweak

--- app/lib/exploration-system.ts
+++ app/lib/exploration-system.ts
@@ -295,8 +295,8 @@ export class ExplorationSystem {
           // Balance distance and recency
           return distanceA + recencyA / 10000 - (distanceB + recencyB / 10000);
         })
-        .slice(0, 16)
-    ); // Limit to top 16 most relevant
+        .slice(0, 14)
+    ); // Limit to top 14 most relevant
   }
 
   // Helper methods
