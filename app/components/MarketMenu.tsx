0e2ca4236003e092b96a4db6f63146820967e905
Alex Woodman <alexthegoodman@gmail.com>
2025-08-23 15:33:59 -0400

    tweak grid

--- app/components/MarketMenu.tsx
+++ app/components/MarketMenu.tsx
@@ -172,7 +172,7 @@ export default function MarketMenu({
       </div>
 
       {/* Building Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
         {currentCategory?.buildings.map((building) => (
           <div
             key={building.type}
